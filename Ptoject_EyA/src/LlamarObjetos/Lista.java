abb9c25357e33f31444b5607aabbb2291d617575
est3ban1006 <67487425+est3ban1006@users.noreply.github.com>
2020-06-29 14:30:21 -0600

    mae no se si hay que crear mas nodos

---
+++ Ptoject_EyA/src/LlamarObjetos/Lista.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LlamarObjetos;
+
+/**
+ *
+ * @author Personal
+ */
+public interface Lista {
+
+    public void insert(Object element);
+
+    public int getcanal(Object element);
+
+    public boolean exists(Object element);
+
+
+}
