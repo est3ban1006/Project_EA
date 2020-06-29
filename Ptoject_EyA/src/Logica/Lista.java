fa35f3a8c86d5a3840ef7116428a01c941aeea0c
est3ban1006 <67487425+est3ban1006@users.noreply.github.com>
2020-06-29 14:37:53 -0600

    mae
    Co-Authored-By: Josué <joarce@users.noreply.github.com>

---
+++ Ptoject_EyA/src/Logica/Lista.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author Personal
+ */
+public interface Lista {
+ 
+    public void insert(Object element);
+
+    public int getCanal(Object element);
+
+    public boolean exists(Object element);
+}
---
+++ Ptoject_EyA/src/Logica/ListaCanales.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author johar
+ */
+public class ListaCanales implements Lista {
+
+    Nodo inicio, fin;
+
+    public ListaCanales() {
+        inicio = fin = null;
+    }
+
+    @Override
+    public void insert(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getCanal(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean exists(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
