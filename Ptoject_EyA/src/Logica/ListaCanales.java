05ae406ca704dfa2b9a57efc27f4ad263b746355
est3ban1006 <67487425+est3ban1006@users.noreply.github.com>
2020-06-29 14:15:04 -0600

    mae no se si asi esta bien o hay que agrargarle mas aunque creo que si
    Co-Authored-By: Josué <joarce@users.noreply.github.com>

--- Ptoject_EyA/src/Logica/ListaCanales.java
+++ Ptoject_EyA/src/Logica/ListaCanales.java
@@ -9,6 +9,27 @@ package Logica;
  *
  * @author johar
  */
-public class ListaCanales {
-    
+public class ListaCanales implements Lista {
+
+    Nodo inicio, fin;
+
+    public ListaCanales() {
+        inicio = fin = null;
+    }
+
+    @Override
+    public void insert(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getCanal(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean exists(Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
